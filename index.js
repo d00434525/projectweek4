bc274a2b2892c9b228780f2a28682680b3b822ea
d00434525 <d00434525@dmail.dixie.edu>
2022-06-24 10:20:24 -0600

    set up index.js file

---
+++ index.js
@@ -0,0 +1,18 @@
+// pull in mongo db
+const mongodb = require("./persist/mongo");
+
+// pull in server/app
+const app = require(".server");
+
+// SETUP CONFIG STUFF
+// flags
+const config = require("./config");
+// set up port number
+
+mongodb.setUpConnectionHandlers(() => {
+    //start server
+    app.listen(config.port, () => {
+        console.log(`Server is running on port ${config.port}`);
+    });
+});
+mongodb.connect();
